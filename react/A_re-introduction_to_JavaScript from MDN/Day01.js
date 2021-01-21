d4843a3fc4b529a2bae2e9ee7517b957d4df5fb2
John Seo <john33fiao@gmail.com>
2021-01-21 15:03:49 +0900

    JS 파일로 재작성

---
+++ react/A_re-introduction_to_JavaScript from MDN/Day01.js
@@ -0,0 +1,8 @@
+// Day01.js
+
+// https://developer.mozilla.org/ko/docs/A_re-introduction_to_JavaScript#.ea.b0.9c.ec.9a.94
+// JavaScript 재입문하기
+// 210121 오전.log 파일 참고
+
+// 현재 문서 완료 후 React로 넘어갈 예정
+
