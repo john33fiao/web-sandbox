ff6228a04ec46f240116a75a8d75970525de42e5
John Seo <john33fiao@gmail.com>
2021-01-25 16:51:45 +0900

    벨로퍼트 모던 리액트

---
+++ react/begin-react/src/Hello.js
@@ -0,0 +1,12 @@
+// Hello.js
+
+import React from 'react';
+// 리엑트 컴포넌트 만들기 위해 리액트 불러옴
+
+function Hello(){
+    return <div>안녕하세요</div>
+}
+
+export default Hello;
+// XML 형식의 값 반환 > JSX
+// Hello 라는 컴포넌트를 내보냄 > 다른 컴포넌트에서 불러와서 사용 가능
