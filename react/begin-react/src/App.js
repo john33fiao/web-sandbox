f56a500a10323847943a4d97a5d7e87d34f99c78
John Seo <john33fiao@gmail.com>
2021-01-25 17:36:41 +0900

    JSX

--- react/begin-react/src/App.js
+++ react/begin-react/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// 현재는 필요 없는 부분이니 일단 삭제
+
+import React from 'react';
+import Hello from './Hello';
 import './App.css';
 
 function App() {
+  const name = 'React';
+  const style = { 
+    // 인라인 style은 JSON으로 작성
+    // 원래 background-color 이면 camelCase로 바꿔서 backgroundColor 처럼 적어주면 됨
+    // 그냥 적응하세요
+    backgroundColor : 'black',
+    color : 'aqua',
+    fontSize: 24, // px 단위가 default
+    padding: '1rem'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Hello
+      // 열리는 태그 내부에는 이렇게도 주석 되기는 함
+      // 근데 야 솔직히 이건 좀 아닌데 에헤이 드러버라
+      />
+      <Hello/>
+      <Hello/>
+      {/* 컴포넌트는 UI 조각이므로 재사용 가능함 */}
+      {/* 주석 넣기 되게 복잡하네 야이씨 */}
+      <div>
+        안녕하세요?
+      </div>
+      <div style={style}>Hello {name}</div>
+      {/* JS 변수 보여줄 때는 {}으로 감싸면 됨 */}
+      <div className="gray-box"></div>
+    </>
+    // 슬래시 주석 가능함
+    // 뭔소리냐면, 위에는 HTML이 아니라 JS라는 뜻임
+    // 하지만 놀랍게도 DIV 안에는 HTML이지
+
+    // 그래서 몇가지 규칙이 있음
+    // 1. 태그 꼭 닫아라, 안닫으면 컴파일 불가능
+    // 심지어 input이나 br 쓸 때도 <br/> 이런 식으로 Self Closing 안해주면 컴파일 불가능
+
+    // 2. 태그는 하나만 사용가능
+    // div*2 이런 거 하면 안되고, div>div*2 이렇게 해야한다는 뜻임
+    // 근데 솔직히 항상 div 쓰면 꼬이거든? 그럴 때는 Fragment 쓰면 됨
+    // <>내용</> 이렇게
+  ); 
+  // 와 근데 저장할 때마다 컴파일 다시 하네?
+  // 심지어 소스보기 해도 안읽힘, JS로 그 위치에 밀어넣어서 그런가?
 }
 
 export default App;
--- react/begin-react/src/index.js
+++ react/begin-react/src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+ReactDOM.render( // 실제 DOM 내부에 리액트 컴포넌트 렌더링 하겠다는 뜻임
+  // public/index에서 ID가 root 인 거 찾아라
   <React.StrictMode>
     <App />
   </React.StrictMode>,
