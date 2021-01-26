2d2113ca3b2db799608fe7fb4c6595fed5472ce2
John Seo <john33fiao@gmail.com>
2021-01-26 17:19:58 +0900

    useRef()로 컴포넌트 안에 변수 만들기

--- react/learn-react/src/App.js
+++ react/learn-react/src/App.js
@@ -1,11 +1,42 @@
 // import logo from './logo.svg';
 import './App.css';
+import React, {useRef} from 'react'
 import Counter from './Counter'
 import InputSample from './InputSample'
 import UserListVer1 from './UserListVer1';
 import UserList from './UserList';
+import UserListVer2 from './UserListVer2';
 
 function App() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
+  const nextId = useRef(4);
+
+  // useRef() 사용할 때 파라미터 넣어주면 이게 .current 의 default값이 됨
+  // 이 값을 기준으로 .currnet 수정/조회하면 됨
+
+  const onCreate = () => {
+    // 나중에 구현 > 배열에 항목 추가하는 로직
+
+    nextId.current +=1;
+  }
+
   return (
     <>
       <Counter/>
@@ -16,6 +47,8 @@ function App() {
       <UserListVer1/>
       <hr/>
       <UserList/>
+      <hr/>
+      <UserListVer2 users = {users}/>
     </>
   );
 }
---
+++ react/learn-react/src/UserListVer2.js
@@ -0,0 +1,36 @@
+// UserListVer2.js
+
+// useRef 는 컴포넌트 안에서 조회 및 수정할 수 있는 변수 관리 목적으로도 사용
+// useRef로 변수 관리하면 값 바뀌어도 컴포넌트 리렌더링 안함
+// 리액트 컴포넌트는 상태 바꾸는 함수 호출 뒤 업데이트된 상태 조회
+// useRef로 관리하면 설정 후 바로 조회
+
+// setTimeout, setInterval로 만든 id
+// 외부 라이브러리로 만든 인스턴스
+// scroll 위치
+
+// 지금까지는 컴포넌트 내부에서 배열 직접 선언해서 사용함
+// 이번에는 App에서 선언한 뒤 UserList(UserListVer2)에게 props로 전달
+
+import React from 'react';
+
+function User({user}){
+    return (
+        <div>
+            <b>{user.username}</b>
+            <span>({user.email})</span>
+        </div>
+    );
+}
+
+function UserListVer2 ({users}){
+    return (
+        <div>
+            {users.map(user => (
+                <User user={user} key={user.id}/>
+            ))}
+        </div>
+    );
+}
+
+export default UserListVer2;
