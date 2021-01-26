3b6edcefa30c4051d57771b14bf55cad3ca21353
John Seo <john33fiao@gmail.com>
2021-01-26 16:06:14 +0900

    focus() DOM API

--- react/learn-react/src/InputSample.js
+++ react/learn-react/src/InputSample.js
@@ -1,12 +1,14 @@
 // InputSample.js
 
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 function InputSample(){
     const [inputs, setInputs] = useState({
         name: '',
         nickname: ''
     });
+
+    const nameInput = useRef();
 
     const {name, nickname} = inputs; // 비구조화 할당으로 값 추출
 
@@ -15,10 +17,12 @@ function InputSample(){
         setInputs({
             ...inputs, // 기존의 input 객체 복사
             // spread 문법, 객체의 내용을 모두 펼쳐서 기존 객체 복사
+
             // 불변성 지키는 것
             // 리액트에서 상태가 업데이트됐음을 감지, 리랜더링 진행
             // 기존 상태 직접 수정하면 값 바꿔도 리랜더링 안함
             // 불변성 지켜야지 컴포넌트 성능 최적화 가능
+
             [name]: value // name키 가진 값을 value로 설정
         });
     };
@@ -28,13 +32,34 @@ function InputSample(){
             name:'',
             nickname:''
         })
-
+        nameInput.current.focus();
     };
+
+    // useRef로 특정 DOM 선택하기
+    // 특히, 외부 라이브러리 사용하는 경우 많이 사용
+    // 리액트에서는 ref로 DOM 선택함 > useRef라는 Hook 함수 사용
+    // 클래스 컴포넌트에서는 React.createRef 함수 쓰는데, 클래스 자체를 잘 안쓰니까 패스(진짜???)
+
+    // 초기화 버튼 클릭하면 input에 포커스 가도록 해봅시다
+
+    // useRef()를 사용해서 Ref 객체 생성
+    // 우리가 선택하려는 DOM에 ref 값으로 설정
+    // .current 값은 우리가 선택하려는 DOM을 가리킴
+    // onReset 함수에서 input에 포커스하는 fucus() DOM API 호출함
 
     return (
         <div>
-            <input name='name' placeholder='이름' onChange={onChange} value={name}/>
-            <input name='nickname' placeholder='닉네임' onChange={onChange} value={nickname}/>
+            <input name='name' 
+            placeholder='이름' 
+            onChange={onChange} 
+            value={name}
+            ref={nameInput}/>
+            
+            <input name='nickname' 
+            placeholder='닉네임' 
+            onChange={onChange} 
+            value={nickname}
+            />
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
